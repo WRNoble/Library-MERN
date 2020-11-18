1de19b0ac15c118d40074ccbd0ae1fd0666cf510
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-11-18 16:24:40 -0500

    made book model

---
+++ models/book.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: { type: String, required: true },
+    published: { type: Number, required: true },
+    description: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
