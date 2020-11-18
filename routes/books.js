4af5594ea2c1ce2928d37873d63937c67e66698f
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-11-18 16:49:56 -0500

    completed routes for books

---
+++ routes/books.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+let Book = require("../models/book.model");
+
+router.route("/").get((req, res) => {
+  Book.find()
+    .then((book) => res.json(book))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/addbook").post((req, res) => {
+  const title = req.body.title;
+  const author = req.body.author;
+  const genre = req.body.genre;
+  const published = Number(req.body.published);
+  const description = req.body.description;
+
+  const newBook = new Book({
+    title,
+    author,
+    genre,
+    published,
+    description,
+  });
+
+  newBook
+    .save()
+    .then(() => res.json("Book Added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  Book.findById(req.params.id)
+    .then((book) => res.json(book))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Book.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Book Deleted :("))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Book.findByIdAndUpdate(req.params.id).then((book) => {
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.genre = req.body.genre;
+    book.published = Number(req.body.published);
+    book.description = req.body.description;
+
+    book
+      .save()
+      .then(() => res.json("Book information has been updated"))
+      .catch((err) => res.status(400).json("Error: " + err));
+  });
+});
+
+module.exports = router;
